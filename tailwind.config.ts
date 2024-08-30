ead9ba1089b5428ebff9fe5b5f9432247f1ceaf6
AndersonGama31 <andersongama31@gmail.com>
2024-08-30 07:13:19 -0300

    chore: Update global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,44 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'background-dark': 'var(--background-dark)',
+        'background': 'var(--background)',
+        'destructive': 'var(--destructive)',
+        'dark': 'var(--dark)',
+        'border': 'var(--border)',
+        'text-dark': 'var(--text-dark)',
+        'pink': 'var(--pink)',
+        'light-pink': 'var(--light-pink)',
       },
+      borderRadius: {
+        'radius-4': 'var(--radius-4)',
+        'radius-8': 'var(--radius-8)',
+        'radius-10': 'var(--radius-10)',
+        'radius-30': 'var(--radius-30)',
+        'radius-70': 'var(--radius-70)',
+        'radius-100': 'var(--radius-100)',
+      },
+      fontSize: {
+        sm: 'var(--font-size-sm)',
+        base: 'var(--font-size-base)',
+        md: 'var(--font-size-md)',
+        lg: 'var(--font-size-lg)',
+        xl: 'var(--font-size-xl)',
+      },
+      lineHeight: {
+        sm: 'var(--line-height-sm)',
+        base: 'var(--line-height-base)',
+        md: 'var(--line-height-md)',
+        lg: 'var(--line-height-lg)',
+        xl: 'var(--line-height-xl)', 
+      },
+      breakpoints: {
+        sm: 'var(--breakpoint-sm)',
+        md: 'var(--breakpoint-md)',
+        lg: 'var(--breakpoint-lg)',
+        xl: 'var(--breakpoint-xl)',
+      }
     },
   },
   plugins: [],
