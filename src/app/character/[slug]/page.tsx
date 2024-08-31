2ba51d2dae63edde267ef4b7a06b4368f2414a56
AndersonGama31 <andersongama31@gmail.com>
2024-08-31 18:10:43 -0300

    feat: Add character page component with comics listing

---
+++ src/app/character/[slug]/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Loader } from 'lucide-react'
+
+import * as Components from '@/components'
+import { useGetCharacterComics } from '@/services/modules/characters/queries'
+
+export default function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params
+  const { data, isLoading, isFetched } = useGetCharacterComics(slug)
+
+  console.log(data)
+
+  return (
+    <main className="flex flex-col items-center h-screen max-w-screen-xl px-20">
+      {isLoading && <Loader className="w-20 h-20 animate-spin mt-10" />}
+
+      {isFetched && (
+        <>
+          <section className="flex w-screen relative">
+            <Components.Parallax />
+          </section>
+
+          <section className="flex flex-col items-center w-full mt-[200px] gap-12">
+            {data?.results?.map(comic => <Components.ComicsCard key={comic.id} {...comic} />)}
+          </section>
+        </>
+      )}
+    </main>
+  )
+}
