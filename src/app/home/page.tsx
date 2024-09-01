06850c784360b358089f65b57a4d2b1a25769024
AndersonGama31 <andersongama31@gmail.com>
2024-09-01 19:29:10 -0300

    refactor: Update responsiviness

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -62,13 +62,13 @@ export default function HomePage() {
   const filteredFavorites = favoriteCharacters?.filter(hero => hero.name.toLowerCase().includes(debouncedValue ?? ''))
 
   return (
-    <main className="flex flex-col items-center h-screen space-y-8 max-w-screen-xl px-4 sm:px-6 lg:px-8 pt-20">
-      <section className="flex flex-col items-center justify-center gap-2 text-center w-full">
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-black text-dark">EXPLORE O UNIVERSO E CRIE SUA EQUIPE</h1>
-        <p className="text-dark opacity-60 mb-5">
+    <main className="flex flex-col items-center h-screen space-y-8 max-w-screen-xl sm:px-6 lg:px-8 pt-20">
+      <section className="flex flex-col items-center justify-center gap-2 text-center w-full px-[50px]">
+        <h1 className="w-full text-3xl lg:text-5xl font-black text-dark">EXPLORE O UNIVERSO E CRIE SUA EQUIPE</h1>
+        <p className="w-full text-dark opacity-60 mb-5 px-4 sm:p-0">
           Os melhores personagens já feitos em quadrinhos. Fique viciado em uma generosa porção de heróis e vilões!
         </p>
-        <div className="flex items-center justify-center w-full sm:w-[780px] mt-2">
+        <div className="flex items-center justify-center w-full px-4 lg:max-w-[780px] sm:px-0 mt-2">
           <Component.Input type="text" name="nameStartsWith" onChange={handleChangeParams} />
         </div>
       </section>
